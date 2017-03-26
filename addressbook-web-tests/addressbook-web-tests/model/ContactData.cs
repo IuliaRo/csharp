f49bcb40a0aa61905fa533829f65048e5e711445
IuliaRomaniuk <iuliia.romaniuk@ticket2travel.ru>
2017-03-26 16:28:32 +0300

    Changed Convert.ToBoolean(LastName.CompareTo(other.lastName)) to(LastName.CompareTo(other.lastName) == 0)

--- addressbook-web-tests/addressbook-web-tests/model/ContactData.cs
+++ addressbook-web-tests/addressbook-web-tests/model/ContactData.cs
@@ -79,7 +79,7 @@ namespace WebAddressbookTests
             {
                 return 1;
             }
-            if (Convert.ToBoolean(LastName.CompareTo(other.lastName))) return FirstName.CompareTo(other.firstName);
+            if (LastName.CompareTo(other.lastName) == 0) return FirstName.CompareTo(other.firstName);
             else
             {
                 return LastName.CompareTo(other.lastName);
