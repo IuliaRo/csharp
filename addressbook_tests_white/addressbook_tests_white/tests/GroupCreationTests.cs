a11bc7532d0b1344b42fa82b47a054a6524830cf
IuliaRomaniuk <iuliia.romaniuk@ticket2travel.ru>
2017-04-28 10:05:05 +0300

    TestGroupCreation - TestGroupRemoving

---
+++ addressbook_tests_white/addressbook_tests_white/tests/GroupCreationTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using System.Linq;
+using System.IO;
+using System.Text.RegularExpressions;
+using NUnit.Framework.Constraints;
+
+namespace addressbook_tests_white
+{
+    [TestFixture]
+    public class GroupCreationTests : TestBase
+    {
+        [Test]
+        public void TestGroupCreation()
+        {
+            List<GroupData> oldGroups = app.Groups.GetGroupList();
+            GroupData newGroup = new GroupData()
+            {
+                Name = "white"
+            };
+
+            app.Groups.AddGroup(newGroup);
+
+            List<GroupData> newGroups = app.Groups.GetGroupList();
+            oldGroups.Add(newGroup);
+
+            oldGroups.Sort();
+            newGroups.Sort();
+
+            NUnit.Framework.Assert.AreEqual(oldGroups, newGroups);
+
+        }
+    }
+}
---
+++ addressbook_tests_white/addressbook_tests_white/tests/GroupRemovingTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using System.Linq;
+using System.IO;
+using System.Text.RegularExpressions;
+using NUnit.Framework.Constraints;
+
+namespace addressbook_tests_white
+{
+    [TestFixture]
+    public class GroupRemovingTests : TestBase
+    {
+        [Test]
+        public void TestGroupRemoving()
+        {
+            List<GroupData> oldGroups = app.Groups.GetGroupList();
+            
+            app.Groups.RemoveGroup(0);
+
+            List<GroupData> newGroups = app.Groups.GetGroupList();
+            oldGroups.Remove(oldGroups[0]);
+
+            oldGroups.Sort();
+            newGroups.Sort();
+
+            NUnit.Framework.Assert.AreEqual(oldGroups, newGroups);
+
+        }
+    }
+}
