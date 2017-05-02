fad4e072f69a9023c52ad4936e9880ff857e9277
IuliaRomaniuk <iuliia.romaniuk@ticket2travel.ru>
2017-05-02 22:08:19 +0300

    9.1 error

---
+++ mantis-tests/mantis-tests/appmanager/ApplicationManager.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace mantis_tests
+{
+    public class ApplicationManager
+    {
+        protected IWebDriver driver;
+        protected string baseURL;
+
+        private static ThreadLocal <ApplicationManager> app = new ThreadLocal<ApplicationManager>(); 
+
+        private ApplicationManager()
+        {
+            driver = new FirefoxDriver(new FirefoxBinary("C:\\Program Files\\Mozilla Firefox\\firefox.exe"), new FirefoxProfile());
+            baseURL = "http://localhost";
+            Registration = new RegistrationHelper(this);
+            Ftp = new FtpHelper(this);
+        }
+
+        public FtpHelper Ftp { get; set; }
+
+        public RegistrationHelper Registration { get; set; }
+
+        ~ApplicationManager()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+        }
+
+        public static ApplicationManager GetInstance()
+        {
+            if (!app.IsValueCreated)
+            {
+                ApplicationManager newInstance = new ApplicationManager();
+                newInstance.driver.Url = "http://localhost/mantisbt-2.4.0/mantisbt-2.4.0/login_page.php";
+                app.Value = newInstance;
+                
+            }
+            return app.Value;
+        }
+
+        public IWebDriver Driver
+        {
+            get { return driver; }
+        }
+    }
+}
---
+++ mantis-tests/mantis-tests/tests/TestBase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+using NUnit.Framework;
+
+namespace mantis_tests
+{
+    public class TestBase
+    {
+        public static bool PERFOM_LONG_UI_CHECKS = true;
+
+        protected ApplicationManager app;
+        
+        [TestFixtureSetUp]
+        public void SetupApplicationManager()
+        {
+            app = ApplicationManager.GetInstance();
+        }
+
+        public static Random rnd = new Random();
+
+        public static string GenerateRandomString(int maximumStringLenght)
+        {
+            int l = Convert.ToInt32(rnd.NextDouble()*maximumStringLenght);
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < l; i++)
+            {
+                builder.Append(Convert.ToChar(65 + Convert.ToInt32(rnd.NextDouble()*90)));
+            }
+            return builder.ToString();
+        }
+    }
+}
